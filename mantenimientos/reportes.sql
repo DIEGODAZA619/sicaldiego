2a9ec076ea8abc2fdb3f3bdd1ea4a93c555c72ef
aldicar <aldidacom@gmail.com>
2023-04-14 22:00:30 -0400

    INICIO

--- mantenimientos/reportes.sql
+++ mantenimientos/reportes.sql
@@ -11,4 +11,3 @@ values (40,76,'RESUMEN VALORADO','Reportes/ResumenValorado','',2,9,3);
 insert into aplicaciones.opciones(id_modulo,codigo_opciones,opcion,link,icono,nivel,orden,id_aplicacion)
 values (40,76,'REPORTES MEFP','Reportes/ReportesMefp','',2,9,3);
 
-aqui
